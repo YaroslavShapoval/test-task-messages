7e1ea06f58676e5c056d4f8e2653582a1e71a471
Yaroslav Shapoval <shapoval.yaroslav@gmail.com>
2016-04-09 20:21:36 +0300

    Add validation for message model

--- source/models/Message.php
+++ source/models/Message.php
@@ -12,10 +12,42 @@ use source\core\BaseModel;
  * @property string $name
  * @property string $email
  * @property string $text
- * @property integer $created_time
+ * @property string $created_at
+ * @property string $updated_at
+ * @property integer $is_approved
  * @property integer $is_edited
  */
 class Message extends BaseModel
 {
     static $table_name = 'test_messages';
+
+    static $validates_presence_of = [
+        ['name',  'Please enter your name'],
+        ['email', 'Please enter your email address'],
+        ['text',  'Please enter message text'],
+    ];
+
+    static $validates_size_of = [
+        [
+            'name',
+            'within' => [3, 30],
+            'too_short' => 'Name cannot be less then 3 symbols length',
+            'too_long' => 'Name cannot be more then 30 symbols length',
+        ],
+
+        [
+            'text',
+            'within' => [3, 255],
+            'too_short' => 'Name cannot be less then 3 symbols length',
+            'too_long' => 'Name cannot be more then 255 symbols length',
+        ],
+    ];
+
+    static $validates_format_of = [
+        [
+            'email',
+            'with' => '/^[^0-9][A-z0-9_]+([.][A-z0-9_]+)*[@][A-z0-9_]+([.][A-z0-9_]+)*[.][A-z]{2,4}$/',
+            'message' => 'Please enter real email address',
+        ],
+    ];
 }
